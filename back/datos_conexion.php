41d6c7be11998568cc7b0462be3daa5212e161c0
AngelGio <85459682+AngelGio@users.noreply.github.com>
2022-03-24 16:03:06 -0500

    Delete datos_conexion.php

--- back/datos_conexion.php
+++
@@ -1,6 +0,0 @@
-<?php    
-	$db_host = "sql308.epizy.com"; //DIRECCION URL
-	$db_nombre = "epiz_31347848_healthy"; // BASE DE DATOS
-	$db_usuario = "epiz_31347848"; //USUARIO DE LA BASE DE DATOS
-	$db_contra = "0PfLNGyicB8q"; //CONTRASEÑA POR DEFECTOS
-?>
